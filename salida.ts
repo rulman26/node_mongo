36981d294bf13136f1c5c52d5b32d1b49bc0ac8d
Rulman Ferro <rulman26@gmail.com>
2019-05-29 17:56:30 -0500

    TypeScrip

---
+++ salida.ts
@@ -0,0 +1,33 @@
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+class Person {
+    protected name: string;
+    constructor(name: string) { this.name = name; }
+}
+
+class Employee extends Person {
+    private department: string;
+
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+let howard = new Employee("Howard", "Sales");
+console.log(howard.getElevatorPitch());
+console.log(howard.name); // error
+
+//para compitar se  tsc salida o tambien version tsc --version
